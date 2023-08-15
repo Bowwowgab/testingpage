f1e5e7eb49ca853acaffda3bc2cebb95114f1cc6
KCode SubField <61689122+Bowwowgab@users.noreply.github.com>
2023-08-15 21:54:04 +0000

    Update unisfa-countdown-clock.js

--- unisfa-countdown-clock.js
+++ unisfa-countdown-clock.js
@@ -1,4 +1,4 @@
-// COUNT DOWN CLCOK
+/ COUNT DOWN CLCOK
 function timeAndDate(){
     let hour = new Date().getHours(), min =  new Date().getMinutes(), sec = new Date().getSeconds(), ampm = '';
     const dayname = new Date().toLocaleString('en-US', {weekday: 'long'}).toUpperCase();
@@ -42,7 +42,7 @@ function forall(dat, obj){
 // CHALK 1 DAYS AND MONEY
 function chalkOneDays(){
   forall('2023-03-29', objchk1);
-  document.querySelector('#ck1').innerHTML = `Days spent: ${objchk1.days}    Days left: ${objchk1.day_left}   Cash: $${objchk1.cash}`;
+  document.querySelector('#ck1').innerHTML = `Days spent: ${objchk1.days}    Days left: ${objchk1.day_left}  Cash: $${objchk1.cash}`;
   localStorage.setItem('ckone', JSON.stringify(objchk1));
  }
 
@@ -52,7 +52,7 @@ function chalkOneDays(){
 // CHALK 2 DAYS AND MONEY
 function chalktwoDays(){
   forall('2023-04-04', objchk2);
-  document.querySelector('#ck2').innerHTML = `Days spent: ${objchk2.days}   Days left: ${objchk2.day_left}  Cash: $${objchk2.cash}`;
+  document.querySelector('#ck2').innerHTML = `Days spent: ${objchk2.days}   Days left: ${objchk2.day_left} Cash: $${objchk2.cash}`;
   localStorage.setItem('cktwo', JSON.stringify(objchk2));
 }
 
@@ -62,9 +62,40 @@ chalktwoDays();
 //CHALK 3 DAYS AND MONEY
 function chalkthreeDays(){
   forall('2023-06-09', objchk3);
-  document.querySelector('#ck3').innerHTML = `Days spent: ${objchk3.days}    Days left: ${ objchk3.day_left}  Cash: $${objchk3.cash}`;
+  document.querySelector('#ck3').innerHTML = `Days spent: ${objchk3.days}    Days left: ${ objchk3.day_left} Cash: $${objchk3.cash}`;
   localStorage.setItem('ckthree', JSON.stringify(objchk3)); 
 }
 
 chalkthreeDays();
+
+
+ let dl =  document.querySelector('#dw-holder-div-btn');
+ let dl_txt = document.querySelector('#dark-light-para');
+ let header = document.querySelector('header');
+ let bn = false;
+ dl.addEventListener('click', ()=>{
+      if(!bn){
+        bn = true;
+       darkView();
+      }else{
+       lightView();
+        bn = false;
+      }
+  });
+  
+function darkView(){
+  dl.classList.add('dark-white-div-style1');
+  dl.classList.remove('dark-white-div-style2');
+  dl_txt.innerHTML = 'Light';
+  document.body.classList.add('body-style');
+  document.querySelector('header').classList.add('header-style');
+}
+
+function lightView(){
+  dl.classList.add('dark-white-div-style2');
+  dl.classList.remove('dark-white-div-style1');
+  dl_txt.innerHTML = 'Dark';
+  document.body.classList.remove('body-style');
+  document.querySelector('header').classList.remove('header-style');
+}
 
