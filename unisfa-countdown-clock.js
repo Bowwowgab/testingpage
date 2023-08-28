62c578483814a10ca7cd149e9503a7da0d2c64f3
KCode SubField <61689122+Bowwowgab@users.noreply.github.com>
2023-08-28 12:11:21 +0000

    Update unisfa-countdown-clock.js

--- unisfa-countdown-clock.js
+++ unisfa-countdown-clock.js
@@ -1,52 +1,52 @@
-// TO FIX THE SWITCH DIV WHEN SCROLL
-window.addEventListener('scroll', ()=>{
-  const sec =  document.querySelector('#second-nav-bar-dark-white-div');
-  const uni =  document.querySelector('#unisfa-txt-div');
-    if(window.scrollY >= 90){
-        sec.style.position = 'fixed';
-        uni.style.visibility = 'visible';
-    }else{
-        sec.style.position = '';
-        uni.style.visibility = 'hidden';
-    }
-});
+//MAKE THE SWITCH DIV FIXED AT THE TOP WHEN SCROLLING
+function scroll(){
+  window.addEventListener('scroll', ()=>{
+    const sec =  document.querySelector('#second-nav-bar-dark-white-div');
+    const uni =  document.querySelector('#unisfa-txt-div');
+      if(window.scrollY > 90){
+          sec.style.position = 'fixed';
+          uni.style.visibility = 'visible';
+      }else{
+          sec.style.position = '';
+          uni.style.visibility = 'hidden';
+      }
+  });
+}
 
+scroll();
 
 // COUNT DOWN CLCOK
 function timeAndDate(){
     let hour = new Date().getHours(), min =  new Date().getMinutes(), sec = new Date().getSeconds(), ampm = '';
-    const dayname = new Date().toLocaleString('en-US', {weekday: 'long'}).toUpperCase();
+    const dayname = new Date().toLocaleString('en-US', {weekday: 'long'});
     let d = new Date().getDate();
     let m = new Date().getMonth() + 1;
     let y = new Date().getFullYear();
 
-    // TIME AM OR PM
+    // DETERMISE AM OR PM FOR THE TIME
     ampm = (hour >= 12)?  ampm = 'PM': ampm = 'AM';
 
-    hour = (hour < 10)? '0' + hour : hour;
-    min  = (min < 10)?  '0' + min  : min;
-    sec  = (sec < 10)?  '0' + sec  : sec;
+    hour = (hour < 10) ? '0' + hour : hour;
+    min  = (min  < 10) ? '0' + min  : min;
+    sec  = (sec  < 10) ? '0' + sec  : sec;
     document.querySelector('#count-down-time').innerText = hour + ':'+ min + ':' + sec + ampm;
 
    //DATE
-    d = (d < 10)? '0' + d : d;
-    m = (m < 10)? '0' + m : m;
-    y = (y < 10)? '0' + y : y;
+    d = (d < 10) ? '0' + d : d;
+    m = (m < 10) ? '0' + m : m;
+    y = (y < 10) ? '0' + y : y;
     document.querySelector('#date').innerText = `${dayname}, ${d}-${m}-${y}`;
     
     setInterval(()=>{timeAndDate()}, 1000);
 }
-
 timeAndDate();
 
 
 // CALCULTE THE DATES DIFFERENCE NOTE: TIME ZONE ISSUE SOLVED BY USING
-//  'T12:00:00Z' OR 'T0:00:00Z' WHICH IS UTC
-// MEANING COODINATE UNIVERSAL TIME
 function calculate_checkday(day){
-  const today = new Date().getTime('T12:00:00Z');
-  const dateofarrival = new Date(day + 'T00:00:00Z').getTime();
-  return Math.round((today - dateofarrival) /(24 * 60 * 60 * 1000));
+  const today = new Date();
+  const dateofarrival = new Date(day + 'T00:00:00');
+  return Math.round((today - dateofarrival) / (24 * 60 * 60 * 1000));
 }
 
 // CALCULTE THE OHTER DETAILS - DAYS SPENTS, LEFT, MONTHS, WEEKS AND REMAINING DAYS
@@ -59,12 +59,12 @@ function forAll_chalk(dateofarrival, ckn){
   
    document.querySelector(ckn).innerHTML = 
    `Days spent: ${dsp}  Days left: ${dll}
-   <p> Total Months: ${m} </p> 
-   <p> Total Weeks: ${w}  </p>
+   <p> Total Months:     ${m} </p> 
+   <p> Total Weeks:      ${w} </p>
    <p> Remaining Day(s): ${r} </p>`;
 }
 
-// FOR CHALK ONE
+// CHALK ONE
 function chalk_1(){
   forAll_chalk('2023-03-30', '#ck1');
 }
@@ -76,7 +76,6 @@ function chalk_2(){
 }
 chalk_2();
 
-
 // CHALK THREE
 function chalk_3(){
   forAll_chalk('2023-06-10', '#ck3');
@@ -84,7 +83,7 @@ function chalk_3(){
 chalk_3();
 
 // TO CHANGE COLOR AND BACKGOUND OF THE PAGE TO DARK OR LIGHT
-let dl =  document.querySelector('#dw-holder');
+let dl = document.querySelector('#dw-holder');
 let bn = false;
 dl.addEventListener('click', ()=>{
      if(!bn){
@@ -98,8 +97,7 @@ dl.addEventListener('click', ()=>{
  
  // DARK ATTRIBUTE
 function darkView(){
-    dl.classList.add('dark-white-div-style1');
-    dl.classList.remove('dark-white-div-style2');
+    dl.style.transform = `rotate(${180}deg)`;
     document.body.classList.add('body-style');
     document.querySelector('.navigational-bar').classList.add('header-style');
     document.querySelector('#second-nav-bar-dark-white-div').classList.add('second-nav-bar-style');
@@ -107,9 +105,9 @@ function darkView(){
 
 // LIGHT ATTRIBRUTE
 function lightView(){
-    dl.classList.add('dark-white-div-style2');
-    dl.classList.remove('dark-white-div-style1'); 
+    dl.style.transform = `rotate(${0}deg)`; 
     document.body.classList.remove('body-style');
     document.querySelector('.navigational-bar').classList.remove('header-style');
     document.querySelector('#second-nav-bar-dark-white-div').classList.remove('second-nav-bar-style');
 }
+
